9aff48b613d76da2e85e8c70ebabe79194dac33f
karnatim <54373400+karnatim@users.noreply.github.com>
2019-08-21 14:30:34 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,27 @@
+/*********************************************
+* Hello Java Program
+**********************************************
+* Project/Program description
+*_____________________________________________
+* Manuroht Karnati
+* 8/21/2019
+* CMSC 255-002
+**********************************************/
+
+public class Hello {
+ public static void printHeading() {
+	System.out.println("Manurohit Karnati");
+	System.out.println("Hello World");
+	System.out.println("Saying Hello!");
+	System.out.println("8/21/2019");
+	System.out.println("CMSC 255-002");
+	System.out.println();
+}
+
+ public static void main(String [] args) {
+	printHeading();
+	System.out.println("Hello");
+	}
+}
+
+	
